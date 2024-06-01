9d855cf83f9b3d266173739a60b654607c0a63e7
Matthias <menderlein@gmx.net>
2024-06-01 18:09:56 +0200

    deleted DemoView.kt implemented ContentView.kt

---
+++ src/main/kotlin/components/ShoppingBasket.kt
@@ -0,0 +1,42 @@
+package components
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShoppingBasket(shoppingMealName: String, count: Int, onCountChange: (Int) -> Unit) {
+    val countMealRemove = remember { mutableStateOf(0) }
+    Row (
+        modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = Color.Gray)
+    ) {
+        Text(
+            style = MaterialTheme.typography.titleLarge,
+            modifier = Modifier.padding(8.dp),
+            text = "$shoppingMealName $count"
+        )
+
+        IconButton(onClick = {
+            countMealRemove.value = 1
+            onCountChange(countMealRemove.value)
+        }) {
+            Icon(imageVector = Icons.Filled.Delete, contentDescription = null)
+        }
+    }
+}
--- src/main/kotlin/views/ContentView.kt
+++ src/main/kotlin/views/ContentView.kt
@@ -1,45 +1,87 @@
 package views
 
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import components.MealView
+import components.ShoppingBasket
 
 @Composable
 fun ContentView() {
-  Row {
-    RowOne()
-    RowTwo()
-    RowThree()
+  var countPizza by remember { mutableStateOf(0) }
+  var countPasta by remember { mutableStateOf(0) }
+
+  val pricePizza = 11.99
+  val pricePasta = 3.99
+
+  val fullPrice: Double = (pricePasta*countPizza)+(pricePasta*countPasta)
+  val formattedFullPrice = String.format("%.2f", fullPrice)
+
+  Row(
+    horizontalArrangement = Arrangement.spacedBy(20.dp)
+
+  ) {
+    Column (
+      verticalArrangement = Arrangement.spacedBy(20.dp)
+    ){
+      // PIZZA
+      MealView(
+        "Pizza",
+        listOf("Tomaten", "Mehl", "Hefe", "Salz", "Wasser", "Olivenöl", "Käse"),
+        onCountChange = { newCount -> countPizza += newCount },
+        pricePizza
+      )
+
+      // PASTA
+      MealView(
+        "Pasta",
+        listOf("Tomaten", "Mehl", "Käse"),
+        onCountChange = { newCount -> countPasta += newCount },
+        pricePasta
+      )
+    }
+
+    Column (
+      verticalArrangement = Arrangement.spacedBy(20.dp)
+    ) {
+      if (countPizza != 0) {
+        Row {
+          ShoppingBasket(
+            "Pizza", count = countPizza,
+            onCountChange = { newCount ->
+              countPizza -= newCount
+            }
+          )
+        }
+      }
+      if (countPasta != 0) {
+        Row {
+          ShoppingBasket(
+            "Pasta", count = countPasta,
+            onCountChange = { newCount ->
+              countPasta -= newCount
+            }
+          )
+        }
+      }
+
+      if (fullPrice != 0.00 ) {
+        Row(
+          modifier = Modifier
+            .clip(RoundedCornerShape(8.dp))
+            .background(color = Color.Green)
+            .padding(8.dp)
+        ) {
+          Text(text = "Price: $formattedFullPrice")
+        }
+      }
+    }
   }
 }
 
-@Composable
-fun RowOne() {
-  Column(
-    modifier = Modifier.padding(5.dp)
-  ) {
-   Text(text = "hihi")
-  }
-}
-
-@Composable
-fun RowTwo() {
-  Column(
-    modifier = Modifier.padding(5.dp)
-  ) {
-    Text(text = "hihi")
-  }
-}
-
-@Composable
-fun RowThree() {
-  Column(
-    modifier = Modifier.padding(5.dp)
-  ) {
-    Text(text = "hihi")
-  }
-}
